e6d01126340573aa8ce1f4a0c8d9b9ee17de993a
Anton.Gorynia <antongorynya1@gmail.com>
2023-05-09 19:10:48 +0300

    Integrate livereload.

--- render_website.py
+++ render_website.py
@@ -1,5 +1,6 @@
 import json
 from jinja2 import Environment, FileSystemLoader
+from livereload import Server, shell
 
 
 def load_template():
@@ -8,14 +9,21 @@ def load_template():
     return index_template
 
 
-
-
-
-if __name__ == '__main__':
+def rebuild():
     with open('books.json', 'r', encoding="utf8") as file:
         context = json.loads(file.read())
     index_template = load_template()
-
-    with open('index.html', mode='w',  encoding='utf8') as result:
+    with open('index.html', mode='w', encoding='utf8') as result:
         result.write(index_template.render(books=context))
+
+
+if __name__ == '__main__':
+    server = Server()
+    rebuild()
+
+    server.watch('templates/*.html', rebuild)
+    server.serve(root='./')
+
+
+
 
